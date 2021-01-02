fd179c017e46403d7631647793cc1e730eef3875
jeremiahcanlas <jeremiahcanlas@gmail.com>
2021-01-01 22:00:20 -0500

    update

--- src/compos/projects.js
+++ src/compos/projects.js
@@ -37,7 +37,6 @@ function Projects() {
             <div className={`project-header fade-in-section ${isVisible ? 'is-visible' : ''}`}
                 ref={domRef}>
                 <h1>Projects</h1>
-                <p>I specialize in solving complex problems with elegantly simple solutions</p>
             </div>
 
             <div className={`project-body fade-in-section ${isVisible ? 'is-visible' : ''}`}
